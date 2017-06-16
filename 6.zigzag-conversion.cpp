fedea3755d6085bb29d1e2888d01298606981c38
yujun <yu.jun.reach@gmail.com>
2017-06-16 21:57:00 +0800

    6:字符串zigzag顺序打印

---
+++ 6.zigzag-conversion.cpp
@@ -0,0 +1,55 @@
+/*
+ * [6] ZigZag Conversion
+ *
+ * https://leetcode.com/problems/zigzag-conversion
+ *
+ * Medium (26.58%)
+ * Total Accepted:    152965
+ * Total Submissions: 575394
+ * Testcase Example:  '""\n1'
+ *
+ * 
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number
+ * of rows like this: (you may want to display this pattern in a fixed font for
+ * better legibility)
+ * 
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * 
+ * 
+ * And then read line by line: "PAHNAPLSIIGYIR"
+ * 
+ * 
+ * Write the code that will take a string and make this conversion given a
+ * number of rows:
+ * 
+ * string convert(string text, int nRows);
+ * 
+ * convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+ * 
+ */
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows <= 1 or numRows >= s.size()) { return s; }
+        vector<vector<char>> v(numRows);
+
+        int i = 0;
+        int inc = 1;
+        for(auto c : s){
+            if(i < 0) { i = 1; inc = 1; }
+            else if(i==numRows) { i = numRows - 2;  inc = -1; }
+            v[i].push_back(c);
+
+            i += inc;
+        }
+
+        string res;
+        for(auto line : v){
+            res += string(line.begin(), line.end());
+        }
+
+        return res;
+    }
+};

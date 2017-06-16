a3c3707ddd49c96f10a854df91ca7686158ebc96
yujun <yu.jun.reach@gmail.com>
2017-06-16 20:53:23 +0800

    525:使用零和来计算最长等次数的01序列

--- 525.contiguous-array.cpp
+++ 525.contiguous-array.cpp
@@ -36,24 +36,26 @@
  */
 class Solution {
 public:
+    //使用栈方式是错误的，[1,0,1,1,0,1]会出错
     int findMaxLength(vector<int>& nums) {
-        stack<pair<int, int>> stk;
-        for(int i=0;i<nums.size();i++) {
-            int num = nums[i];
-            if(stk.empty()||stk.top().first==num) { stk.push(make_pair(num, i)); }
-            else { stk.pop(); }
+        for(int i=0; i<nums.size(); i++) { 
+            if(!nums[i]) nums[i] = -1;
         }
 
+        map<int, int> m;
+        int sum = 0;
         int max = 0;
-        int last = nums.size();
-        while(!stk.empty()) {
-            int i = stk.top().second;
-            printf("(%d,%d)", i, nums[i]);
-            max = max > (last-i-1) ? max : (last-i-1);
-            last = i;
-            stk.pop();
+        m[0] = -1;
+
+        for(int i=0;i<nums.size();i++){
+            sum += nums[i];
+            if(m.count(sum)){
+                max = max > (i - m[sum]) ? max : (i-m[sum]);
+            }
+            else{
+                m[sum] = i;
+            }
         }
-        max = max > last ? max : last;
 
         return max;
     }

ca58ab8f7f9b66b3e7c7db0d83c2558a464464b4
yujun <yu.jun.reach@gmail.com>
2017-06-20 20:36:31 +0800

    449:二叉搜索树序列化和反序列化

---
+++ 449.serialize-and-deserialize-bst.cpp
@@ -0,0 +1,83 @@
+/*
+ * [449] Serialize and Deserialize BST
+ *
+ * https://leetcode.com/problems/serialize-and-deserialize-bst
+ *
+ * Medium (42.00%)
+ * Total Accepted:    12627
+ * Total Submissions: 30052
+ * Testcase Example:  '[2,1,3]'
+ *
+ * Serialization is the process of converting a data structure or object into a
+ * sequence of bits so that it can be stored in a file or memory buffer, or
+ * transmitted across a network connection link to be reconstructed later in
+ * the same or another computer environment. 
+ * 
+ * Design an algorithm to serialize and deserialize a binary search tree. There
+ * is no restriction on how your serialization/deserialization algorithm should
+ * work. You just need to ensure that a binary search tree can be serialized to
+ * a string and this string can be deserialized to the original tree
+ * structure.
+ * 
+ * 
+ * The encoded string should be as compact as possible.
+ * 
+ * 
+ * 
+ * Note: Do not use class member/global/static variables to store states. Your
+ * serialize and deserialize algorithms should be stateless.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        if(!root) { return ""; }        
+
+        return to_string(root->val) + "," + serialize(root->left) + serialize(root->right);
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        size_t i = 0, j;
+        stack<TreeNode*> stk;
+        TreeNode* root = NULL;
+        while((j=data.find(',', i))!=string::npos){
+            TreeNode* p = new TreeNode(atoi(data.substr(i, j - i).c_str()));
+            if(!root){
+                root = p;
+            }
+            else if(stk.top()->val >= p->val) { 
+                stk.top()->left = p;
+            }
+            else{
+                TreeNode* q;
+                while(!stk.empty() && stk.top()->val < p->val){
+                    q = stk.top();
+                    stk.pop();
+                }
+                q->right = p;
+            }
+
+            stk.push(p);
+
+            i = j + 1;
+        }
+
+        return root;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec;
+// codec.deserialize(codec.serialize(root));

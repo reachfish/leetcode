706d392d07bffa386d2ed9797bc9e55177e06ed4
yujun <yu.jun.reach@gmail.com>
2017-08-01 12:44:24 +0800

    646:求不相交区间的最多个数

---
+++ 646.maximum-length-of-pair-chain.cpp
@@ -0,0 +1,61 @@
+/*
+ * [646] Maximum Length of Pair Chain
+ *
+ * https://leetcode.com/problems/maximum-length-of-pair-chain
+ *
+ * algorithms
+ * Medium (45.95%)
+ * Total Accepted:    3.9K
+ * Total Submissions: 8.6K
+ * Testcase Example:  '[[1,2], [2,3], [3,4]]'
+ *
+ * 
+ * You are given n pairs of numbers. In every pair, the first number is always
+ * smaller than the second number.
+ * 
+ * 
+ * 
+ * Now, we define a pair (c, d) can follow another pair (a, b) if and only if b
+ * < c. Chain of pairs can be formed in this fashion. 
+ * 
+ * 
+ * 
+ * Given a set of pairs, find the length longest chain which can be formed. You
+ * needn't use up all the given pairs. You can select pairs in any order.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: [[1,2], [2,3], [3,4]]
+ * Output: 2
+ * Explanation: The longest chain is [1,2] -> [3,4]
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * The number of given pairs will be in the range [1, 1000].
+ * 
+ * 
+ */
+class Solution {
+public:
+	static bool compare(const vector<int>& l, const vector<int>& r){
+		return l[1] < r[1];
+	}
+
+    int findLongestChain(vector<vector<int>>& pairs) {
+		sort(pairs.begin(), pairs.end(), Solution::compare); 
+		int res = 0;
+		int last = -1;
+		for(int i=0; i<pairs.size(); i++){
+			if(last==-1 || pairs[i][0] > pairs[last][1]){
+				res++;
+				last = i;
+			}
+		}
+
+		return res;
+    }
+};

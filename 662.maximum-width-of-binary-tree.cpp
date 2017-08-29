d262c5d001799d6e45821a7b7c48ed3d3e1dbdad
yujun <yu.jun.reach@gmail.com>
2017-08-29 20:20:57 +0800

    662:求树的最大宽度

---
+++ 662.maximum-width-of-binary-tree.cpp
@@ -0,0 +1,127 @@
+/*
+ * [662] Maximum Width of Binary Tree
+ *
+ * https://leetcode.com/problems/maximum-width-of-binary-tree
+ *
+ * algorithms
+ * Medium (34.96%)
+ * Total Accepted:    3.4K
+ * Total Submissions: 9.7K
+ * Testcase Example:  '[1,3,2,5,3,null,9]'
+ *
+ * Given a binary tree, write a function to get the maximum width of the given
+ * tree. The width of a tree is the maximum width among all levels. The binary
+ * tree has the same structure as a full binary tree, but some nodes are
+ * null. 
+ * 
+ * The width of one level is defined as the length between the end-nodes (the
+ * leftmost and right most non-null nodes in the level, where the null nodes
+ * between the end-nodes are also counted into the length calculation.
+ * 
+ * Example 1:
+ * 
+ * Input: 
+ * 
+ * ⁠          1
+ * ⁠        /   \
+ * ⁠       3     2
+ * ⁠      / \     \  
+ * ⁠     5   3     9 
+ * 
+ * Output: 4
+ * Explanation: The maximum width existing in the third level with the length 4
+ * (5,3,null,9).
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: 
+ * 
+ * ⁠         1
+ * ⁠        /  
+ * ⁠       3    
+ * ⁠      / \       
+ * ⁠     5   3     
+ * 
+ * Output: 2
+ * Explanation: The maximum width existing in the third level with the length 2
+ * (5,3).
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * Input: 
+ * 
+ * ⁠         1
+ * ⁠        / \
+ * ⁠       3   2 
+ * ⁠      /        
+ * ⁠     5      
+ * 
+ * Output: 2
+ * Explanation: The maximum width existing in the second level with the length
+ * 2 (3,2).
+ * 
+ * 
+ * Example 4:
+ * 
+ * Input: 
+ * 
+ * ⁠         1
+ * ⁠        / \
+ * ⁠       3   2
+ * ⁠      /     \  
+ * ⁠     5       9 
+ * ⁠    /         \
+ * ⁠   6           7
+ * Output: 8
+ * Explanation:The maximum width existing in the fourth level with the length 8
+ * (6,null,null,null,null,null,null,7).
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * Answer will in the range of 32-bit signed integer.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+
+		int max = 0;        
+		vector<int> v;
+		help(root, 0, 1, v, max);
+
+		return max;
+    }
+
+	void help(TreeNode* p, int depth, int index, vector<int>& v, int& max){
+		if(!p){
+			return;
+		}
+
+		if(depth==v.size()){
+			v.push_back(index);
+		}
+
+		int width = index - v[depth] + 1;
+		if(width > max){
+			max = width;
+		}
+
+		help(p->left, depth + 1, index * 2 - 1, v, max);
+		help(p->right, depth + 1, index * 2, v, max);
+	}
+};

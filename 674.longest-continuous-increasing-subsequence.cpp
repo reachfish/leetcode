3477d56dda8fd0eb1577324374bcaae22b3bb03e
yujun <yu.jun.reach@gmail.com>
2017-11-03 08:52:44 +0800

    674:求递增的连续子数列的最大长度

---
+++ 674.longest-continuous-increasing-subsequence.cpp
@@ -0,0 +1,65 @@
+/*
+ * [674] Longest Continuous Increasing Subsequence
+ *
+ * https://leetcode.com/problems/longest-continuous-increasing-subsequence
+ *
+ * algorithms
+ * Easy (42.96%)
+ * Total Accepted:    14.5K
+ * Total Submissions: 33.9K
+ * Testcase Example:  '[1,3,5,4,7]'
+ *
+ * 
+ * Given an unsorted array of integers, find the length of longest continuous
+ * increasing subsequence.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: [1,3,5,4,7]
+ * Output: 3
+ * Explanation: The longest continuous increasing subsequence is [1,3,5], its
+ * length is 3. 
+ * Even though [1,3,5,7] is also an increasing subsequence, it's not a
+ * continuous one where 5 and 7 are separated by 4. 
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: [2,2,2,2,2]
+ * Output: 1
+ * Explanation: The longest continuous increasing subsequence is [2], its
+ * length is 1. 
+ * 
+ * 
+ * 
+ * Note:
+ * Length of the array will not exceed 10,000.
+ * 
+ */
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+		if(nums.empty()){
+			return 0;
+		}
+
+		int len = 1;
+		int max = len;
+
+		for(int i=1; i<nums.size(); i++){
+			if(nums[i] > nums[i-1]){
+				len++;
+				if(len > max){
+					max = len;
+				}
+			}
+			else{
+				len = 1;
+			}
+		}
+
+		return max;
+    }
+};

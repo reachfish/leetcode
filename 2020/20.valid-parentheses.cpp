72f6189edf2c0011e0f872359bd7e4226f05e1d2
yujun <yu.jun.reach@gmail.com>
2020-06-18 07:24:33 +0800

    20.valid-parentheses

---
+++ 2020/20.valid-parentheses.cpp
@@ -0,0 +1,108 @@
+/*
+ * @lc app=leetcode id=20 lang=cpp
+ *
+ * [20] Valid Parentheses
+ *
+ * https://leetcode.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (38.66%)
+ * Likes:    4896
+ * Dislikes: 216
+ * Total Accepted:    992.4K
+ * Total Submissions: 2.6M
+ * Testcase Example:  '"()"'
+ *
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and
+ * ']', determine if the input string is valid.
+ * 
+ * An input string is valid if:
+ * 
+ * 
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * 
+ * 
+ * Note that an empty string is also considered valid.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "()"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "()[]{}"
+ * Output: true
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "(]"
+ * Output: false
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: "([)]"
+ * Output: false
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input: "{[]}"
+ * Output: true
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isValid(string s) {
+		stack<char> sc;
+		for (auto c : s) {
+			switch (c) {
+				case '(':
+				case '[':
+				case '{': {
+					sc.push(c);
+					break;
+				}
+				case ')': {
+					if (sc.empty() || sc.top() != '(') {
+						return false;
+					}
+					sc.pop();
+					break;
+				}
+				case ']': {
+					if (sc.empty() || sc.top() != '[') {
+						return false;
+					}
+					sc.pop();
+					break;
+				}
+				case '}': {
+					if (sc.empty() || sc.top() != '{') {
+						return false;
+					}
+					sc.pop();
+					break;
+				}
+				default: {
+					return false;
+				}
+			}
+		}
+
+		return sc.empty();
+    }
+};
+// @lc code=end

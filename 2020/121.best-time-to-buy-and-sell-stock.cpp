dddd494c38c4d9e4aa8ccb784461e2253576dee6
reachfish <yu.jun.reach@gmail.com>
2021-02-09 19:47:45 +0800

    121.best-time-to-buy-and-sell-stock

---
+++ 2020/121.best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode id=121 lang=cpp
+ *
+ * [121] Best Time to Buy and Sell Stock
+ *
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+ *
+ * algorithms
+ * Easy (51.38%)
+ * Likes:    7543
+ * Dislikes: 337
+ * Total Accepted:    1.2M
+ * Total Submissions: 2.3M
+ * Testcase Example:  '[7,1,5,3,6,4]'
+ *
+ * You are given an array prices where prices[i] is the price of a given stock
+ * on the i^th day.
+ * 
+ * You want to maximize your profit by choosing a single day to buy one stock
+ * and choosing a different day in the future to sell that stock.
+ * 
+ * Return the maximum profit you can achieve from this transaction. If you
+ * cannot achieve any profit, return 0.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: prices = [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit
+ * = 6-1 = 5.
+ * Note that buying on day 2 and selling on day 1 is not allowed because you
+ * must buy before you sell.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: prices = [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transactions are done and the max profit =
+ * 0.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= prices.length <= 10^5
+ * 0 <= prices[i] <= 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty()) { return 0; }
+
+        int mx        = prices.back();
+        int mx_profit = 0;
+        for (int i = prices.size() - 2; i >= 0; i--) {
+            int p = prices[i];
+            if (mx < p) {
+                mx = p;
+            } else {
+                int profit = mx - p;
+                if (profit > mx_profit) { mx_profit = profit; }
+            }
+        }
+
+        return mx_profit;
+    }
+};
+// @lc code=end
